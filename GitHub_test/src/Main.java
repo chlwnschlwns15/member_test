461879915be403af582d8802e77ae113d1ecc413
smhrc12 <smhrc12@DESKTOP-6DP4RB5>
2018-05-18 11:47:59 +0900

    main 작없중

--- GitHub_test/src/Main.java
+++ GitHub_test/src/Main.java
@@ -37,6 +37,20 @@ public class Main {
 		MemberDAO dao = new MemberDAO();
 		Member m = dao.Delete(memNum);
 		System.out.println(memNum+"이 삭제되었습니다");
+		
+		
+		}else if(choice == 4) {
+			System.out.print("수정하고 싶은 memNum을 입력해주세요 >> ");
+			String memNum = s.next();
+			System.out.print("이름 >> ");
+			String name = s.next();
+			System.out.print("나이 >> ");
+			int age = s.nextInt();
+			
+			System.out.print("전화번호 >> ");
+			String tel = s.next();
+			
+			
 		}
 	}
 	
